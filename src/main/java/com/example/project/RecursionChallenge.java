20160d2350bf0f25ab42c5d5ee302965ead62efc
Willem225 <Waiy2@nycstudents.net>
2025-04-09 13:35:56 +0000

    real

--- src/main/java/com/example/project/RecursionChallenge.java
+++ src/main/java/com/example/project/RecursionChallenge.java
@@ -2,45 +2,82 @@
 
     public class RecursionChallenge {
 
-            public static int bunnyEars(int n){
+        public static int bunnyEars(int n){
+            if (n == 0) {
+                return 0;
+            }
+            return 2 + bunnyEars(n -  1);
+        }
+    
+        public static int factorial(int n){
+            if (n == 1) {
                 return 1;
             }
-        
-            public static int factorial(int n){
+            return n * factorial(n - 1);
+        }
+
+        public static int sumNumbers(int n){
+            if (n == 1) {
                 return 1;
             }
+            return n + sumNumbers(n - 1);
+        }
 
-            public static int sumNumbers(int n){
+        public static String countDown(int n){
+            if (n == 0) {
+                return "Blast Off!";
+            }
+            countDown(n - 1);
+            return n + "!";
+        }
+
+        public static int power(int x, int n){
+            if (n == 0) {
                 return 1;
             }
+            return x * power(x, n - 1);
+        }
 
-            public static String countDown(int n){
-                return "";
+        public static int fib(int n){
+            if (n <= 1) {
+                return n;
             }
+            return fib(n - 1) + fib(n - 2);
+        }
 
-            public static int power(int x, int n){
-                return 1;
+        public static int countX(String s){
+            if (s.isEmpty()) {
+                return 0;
             }
+            return (s.charAt(0) == 'x' ? 1 : 0)  + countX(s.substring(1));
+        }
 
-            public static int fib(int n){
-                return 1;
+        public static String changePi(String s){
+            if (s.length() < 2) {
+                return s;
             }
-
-            public static int countX(String s){
-                return 1;
+            if (s.startsWith("pi")) {
+                return "3.14" + changePi(s.substring(2));
             }
+            return s.charAt(0) + changePi(s.substring(1));
+        }
 
-            public static String changePi(String s){
-                return "";
+
+        public static String reverse(String s){
+            if (s.length() <= 1) { 
+                return s;
             }
+            return reverse(s.substring(1)) + s.charAt(0);
+        }
 
-
-            public static String reverse(String s){
-                return "";
+        public static Boolean isPalindrome(String s){
+            if (s.length() <= 1) {
+                return true;
             }
-
-            public static Boolean isPalindrome(String s){
+            if (s.charAt(0) != s.charAt(s.length() - 1)) { 
                 return false;
             }
+            return (isPalindrome(s.substring(1, s.length() - 1)));
+        }
+}
 
-    }
